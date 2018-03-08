a011ca1a314c9850a6139e3e67311b97302d6892
CrunchyJohnHaven <johnhavenbradley@gmail.com>
2018-03-08 04:12:59 -0800

    primitives commit

--- graphicsPrimitives.playground/Contents.swift
+++ graphicsPrimitives.playground/Contents.swift
@@ -3,18 +3,37 @@
 import UIKit
 
 //Create a Point struct that has two properties called "X" & "Y" both of which are Doubles
-struct Point {
+class Point {
     var X: Double
     var Y: Double
+    init(X: Double, Y: Double) {
+        self.X = X
+        self.Y = Y
+    }
 }
 let thisPoint = Point(X: 1.0, Y: 2.0)
-
+print(thisPoint.X)
+//
 //Create a Line struct that has a "Start" and "End" property both of type Point
-struct Line {
-    var Start: Point
-    var End: Point
+class Line: Point {
+    let Start: [Double]
+    let End: [Double]
+    init(Start: [Double], End: [Double], X: Double, Y: Double) {
+        self.Start = Start
+        self.End = End
+        super.init(X: X, Y:Y)
+    }
+    func length() -> Int {
+        let length = Int(self.Y - self.X)
+        return length
+    }
 }
-var thisLine = Line(Start: 10, End: 20)
+var LineOne = Line(Start: [3.0], End: [4.0], X: 3.0, Y: 4.0)
+var len = LineOne.length()
+print("Length", len)
+
+
+
 
 //In the Line struct add a function to return the Length of the created line as a Double.
 
@@ -38,6 +57,20 @@ class Person {
     }
 }
 var myPerson: Person = Person(name: "Haven")
+
+class Developer: Person {
+    var favoriteLanguage: String
+    init(name: String, favoriteLanguage: String) {
+        self.favoriteLanguage = favoriteLanguage
+        super.init(name: name)
+    }
+    override func speak() {
+        print("Hello! I am a Developer! My name is \(self.name)")
+    }
+}
+//DEVELOPER
+var myDeveloper: Developer = Developer(name: "John", favoriteLanguage: "Swift")
+myDeveloper.speak()
 
 
 //STORED PROPERTIES
